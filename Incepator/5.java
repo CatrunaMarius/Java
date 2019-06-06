b2ed705341918d35dbe77be6ace8b2235411c41d
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-06 15:19:43 +0300

    Create 5.java

---
+++ Incepator/5.java
@@ -0,0 +1,9 @@
+class scanner{
+     public static void main(String args[]){
+          
+          int mars = 5;
+          mars = mars +5//  will increase by 5        
+          
+          System.out.println(mars);
+          }
+ }  
