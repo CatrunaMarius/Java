c40cae17b99dccf7fc514917ebe13baf3dc0fe25
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-06 19:22:18 +0300

    Create 20 array for.java

---
+++ Incepator/20 array for.java
@@ -0,0 +1,10 @@
+class apples{
+    public static void main(String[] args) {
+          System.out.println("Index\tValue");
+          int array [] = {32,12,18,54,2};
+          
+          for (int counter=0; counter<array.length;counter++){
+                System.out.println(counter + "\t" + array[counter]);
+          }
+     }
+}
