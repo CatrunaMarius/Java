af1dfe54053cc375dc874d5781767ef10d32b573
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-06 14:36:06 +0300

    Create Scanner-2.java

---
+++ Incepator/Scanner-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class scanner{
+     public static void main(String args[]){
+          Scanner mars = new Scanner (System.in);
+          double fnum, snum, answer;
+          System.out.println("Entre first num: ");
+          fnum = mars.nextDouble();
+          System.out.println("Entre second num: ");
+          snum = mars.nextDuble();
+          answer = fnum + snum;
+          System.out.println(answer);
+          }
+ }         
