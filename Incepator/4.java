d8c5b05b717adb34b19882c147452a7a77d1a6f4
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-06 14:56:42 +0300

    Create 4.java

---
+++ Incepator/4.java
@@ -0,0 +1,13 @@
+
+
+class scanner{
+     public static void main(String args[]){
+          
+          
+          
+          int mars1 = 5;
+          int bass = 18;
+          ++mars1;
+          System.out.println(mars1);
+          }
+ }  
