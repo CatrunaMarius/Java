ea69653ea398cebac306a06fd4e94f035e224928
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-06 16:05:44 +0300

    Create 8.java

---
+++ Incepator/8.java
@@ -0,0 +1,22 @@
+package apples;
+class apples{
+    public static void main (String args[]){
+       int age;
+       age =3;
+       
+       switch (age){
+       case 1:
+            System.out.println("You can cry");
+            break;
+       case 2:
+            System.out.println("You can talk");
+            break;
+       case 3:
+            System.out.println("You can get in toruble");
+            break;
+       default:
+            System.out.println("I dont know how old you are?");
+       break;
+       }
+    }   
+}
