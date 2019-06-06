049b744bdc63c40db97de383d3cd1b2a77ff7356
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-06 16:55:57 +0300

    Create mars.java

---
+++ Incepator/class/app4/mars.java
@@ -0,0 +1,19 @@
+public class mars{
+   private String girlName;
+    
+   public mars (String name){
+       girlName= name;
+   }
+   
+   public void setName(String name){
+        girlName=name;
+   }
+   
+   public String getName(){
+         return girlName;
+   }
+   
+   public void Saying (){
+        System.out.printf("Your first gf was %s\n, getName());
+    }
+ }
