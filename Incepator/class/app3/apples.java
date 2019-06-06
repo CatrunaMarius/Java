f7476ac99c8fb2520c3267e93055abd84db12e9b
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-06 16:47:15 +0300

    Create apples.java

---
+++ Incepator/class/app3/apples.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class apples{
+      public static void main (String [] args ){
+            Scanner input = new Scanner (System.in);
+            mars marsObject = new mars();
+            System.out.println("Entre name of first gf here: ");
+            String temp input.nextLine();
+            tunaObject.setName (temp);
+            marsObject.saying();
+         }
+ }
