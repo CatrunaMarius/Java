e7e2baf083fcaaa319486c9e81eda6e46dbd914d
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-06 15:46:55 +0300

    Create 7.java

---
+++ Incepator/7.java
@@ -0,0 +1,14 @@
+class apples{
+  public static void main (String args[]){
+        int boy, girl;
+        boy = 18;
+        girl =68;
+        
+        if (boy >10){
+             System.out.println("You can entre");
+             }
+        else{
+             System.out.println("You are too young");
+             }
+        }
+ }       
