fedf417fbfe1e3031e581eeb4c55d2a3fe5bb4ee
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-06 15:29:00 +0300

    Update 5.1.java

--- Incepator/5.1.java
+++ Incepator/5.1.java
@@ -2,7 +2,10 @@ class scanner{
      public static void main(String args[]){
           
           int mars = 5;
-          mars += 8  //will increase by 8       
+          mars += 8  //will increase by 8 
+          //mars -=8 //will decrease by 8
+          //mars *= 8 //will multiply by 8
+          //mars /=8 //will divide by 8
           
           System.out.println(mars);
           }
