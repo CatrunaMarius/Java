3a2697a0b4b770374b3074b48894e32dcc734e50
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-06 19:06:15 +0300

    Create 18....java

---
+++ Incepator/18....java
@@ -0,0 +1,11 @@
+class apples{
+   public static void main(String[] args){
+       int mars [] = new int [10];
+       
+       mars[0] =87;
+       mars[1] = 543;
+       mars[2] = 65;
+       
+       System.out.println(mars[1];
+   }
+}
