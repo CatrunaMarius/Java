9b1a2238ca3e5c9ce4565f19d6f0ed5d0b8b163b
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 22:22:27 +0300

    Update apples.java

--- Incepator/class/JComboBox/apples.java
+++ Incepator/class/JComboBox/apples.java
@@ -1,1 +1,16 @@
+package apples;
 
+import javax.swing.JFrame;
+
+class apples{
+	public static void main (String[] args) {
+		
+		GUi go = new GUi();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go.setSize(300,200);
+		go.setVisible(true);
+		
+		
+	}
+	
+}
