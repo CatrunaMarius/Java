814cc7f9088d96c010e716bf5290b45da14df535
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 14:26:53 +0300

    Create mars.java

---
+++ Incepator/class/enumeration/mars.java
@@ -0,0 +1,22 @@
+public enum mars{
+   marius("nice", "25"),
+   ioana("cutie", "10"),
+   maria("bigmistake", "12");
+   
+   private final String desc;
+   private final String year;
+   
+   mars(String description, String birthday){
+           desc = description;
+           year = birthday;
+   
+   }
+   
+   public String getdesc(){
+        return desc;
+   }
+   
+   public String getYear(){
+        return year;
+   }
+   
