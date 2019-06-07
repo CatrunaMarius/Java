4908032be5c0cb7f74942c3e36a925afa6e07fea
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 16:25:55 +0300

    Update apples.java

--- Incepator/class/inheritance/apples.java
+++ Incepator/class/inheritance/apples.java
@@ -1,1 +1,11 @@
 
+class apples{
+    public static void main(String[] args){
+      mars marsObject =new mars ();
+      food foodObject = new food();
+      
+      marsObject.eat();
+      foodObject.eat();
+      
+    }
+}
