98bed12cea0966043110e0ab7b1d600efac33883
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 11:35:57 +0300

    Create apples.java

---
+++ Incepator/class/app/apples.java
@@ -0,0 +1,8 @@
+class apples{
+     public static void main (String [] args){
+         mars marsObject = new mars();
+         System.out.println(marsObject.toMilitary());
+         marsObject.setTime(13,27,16);
+         System.out.println(marsObject.toMilitary());
+      }
+ }
