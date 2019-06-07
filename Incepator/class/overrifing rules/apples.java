1ad0195d3fcfcf0b0c1462296fe49da8e3a46507
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 18:26:35 +0300

    Create apples.java

---
+++ Incepator/class/overrifing rules/apples.java
@@ -0,0 +1,17 @@
+package apples;
+//overriding rules
+
+
+class apples{
+	public static void main (String[] args) {
+		
+		fatty marius = new fatty();
+		food fo = new food();
+		food da = new data();
+		
+		marius.digest(fo);
+		marius.digest(da);
+		
+	}
+	
+}
