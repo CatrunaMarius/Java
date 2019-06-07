0a6847f2a3d51eb11951d9974bf7de296df3e9d6
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 14:52:23 +0300

    Create mars.java

---
+++ Incepator/class/app7/mars.java
@@ -0,0 +1,13 @@
+public class mars{
+   private String first;
+   private last;
+   private static int members = 0;
+   
+   public mars (String fn, String ln){
+       first = fn;
+       last = ln;
+       members++;
+       System.out.printf("Constructor for %s %s, members in the club: %d\n", first, last, members)
+   
+   }
+}
