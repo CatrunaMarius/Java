31e960d515eb4acf06827b786dcc5f29f403deea
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 14:46:05 +0300

    Update apples.java

--- Incepator/class/enumeration/apples.java
+++ Incepator/class/enumeration/apples.java
@@ -1,6 +1,14 @@
+import java.util.EnumSet;
+
 class apples{
     public static void main(String[] args){
        for(mars people : mars.values())
-            System.out.printf("%s\t%s\t%s\n", people, people.getDesc(), people.getYear);
+            System.out.printf("%s\t%s\t%s\n", people, people.getDesc(), people.getYear());
+    
+            System.out.println("\nAnd now for the range of constants!!!\n");        
+        
+            for( mars people: EnumSet.range(mars.ioana, mars.maria))
+            	        System.out.printf("%s\t%s\t%s\n", people, people.getDesc(), people.getYear());
+    
     }
 }
