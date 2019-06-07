8d5f3678e3a8fe77cd0a3a1148bd935ab4ca26d4
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 16:10:36 +0300

    Create apples.java

---
+++ Incepator/class/app8/apples.java
@@ -0,0 +1,10 @@
+class apples{
+    public static void main(String[] args){
+      mars marsObject =new mars (10);
+      
+      for(int i=0; i<5; i++) {
+    	  marsObject.add();
+    	  System.out.printf("%s", marsObject);
+      }
+    }
+}
