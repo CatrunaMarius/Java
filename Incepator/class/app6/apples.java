b1b9455f426df09d58ac5c00d1823c9c95cf564d
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 14:16:38 +0300

    Update apples.java

--- Incepator/class/app6/apples.java
+++ Incepator/class/app6/apples.java
@@ -1,7 +1,7 @@
 class apples{
     public static void main (String [] args){
-       data potObject = new data(07,21,1994);
-       mars marsObject = new mars("Marius", potObject);
+       data dataObject = new data(07,21,1994);
+       mars marsObject = new mars("Marius", dataObject);
        
        System.out.println(marsObject);
     }
