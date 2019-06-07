adae069c62d154c787fc5aa011ff99cbcbc423ac
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 17:39:12 +0300

    Create apples.java

---
+++ Incepator/class/JFrame/apples.java
@@ -0,0 +1,14 @@
+package apples;
+
+import javax.swing.JFrame;
+
+class apples{
+	public static void main (String[] args) {
+		
+		mars marius = new mars();
+		marius.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		marius.setSize(275,180);
+		marius.setVisible(true);
+	}
+	
+}
