764a39afe58f8eb5bfcd1e2b0abc7b6cc93a8e5c
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-07 15:08:05 +0300

    Create apples.java

---
+++ Incepator/class/app7/apples.java
@@ -0,0 +1,11 @@
+class apples{
+    public static void main(String[] args){
+      mars member1 =new mars ("Megan", "Fox");
+      mars member2 =new mars ("Natalie", "Fox");
+      
+      System.out.println();
+      System.out.println(member2.getFirst());
+      System.out.println(member2.getLast());
+      System.out.println(member2.getMembrs());
+    }
+}
