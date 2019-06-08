fb94a8b0d91cc042f9c2863e78fa296ab0c55edc
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-08 15:44:06 +0300

    Create apples.java

---
+++ Incepator/class/read txt/apples.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class apples{
+    public static void main (String [] args){
+       readfile r = new readfile();
+       r.openFile();
+       r.readFile();
+       r.closeFile();
+       
+    }
+} 
