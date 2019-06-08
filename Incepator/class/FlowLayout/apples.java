a02a3ed69b22c4102cdfd84fcfe5f657e121e17e
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-08 17:33:45 +0300

    Create apples.java

---
+++ Incepator/class/FlowLayout/apples.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+
+
+class apples{
+	public static void main (String[] args) {
+		
+		Layout go = new Layout();
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go.setSize(300,200);
+		go.setVisible(true);
+	
+			}
+	}
