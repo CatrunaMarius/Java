0f4a1240063699f76ef10c1648adbb775db636a5
Cătrună Marius Robert <47449655+CatrunaMarius@users.noreply.github.com>
2019-06-08 18:22:13 +0300

    Create apples.java

---
+++ Incepator/class/JColorChooser/apples.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+
+
+class apples{
+	public static void main (String[] args) {
+		
+		
+		Peach p = new Peach();
+		p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+	}
+	
+}
